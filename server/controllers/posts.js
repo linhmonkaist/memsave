8d4ddfd6a6a050ca38a767d8fa711d1f1c7f402b
linhmonkaist <nguyenlinhmon@gmail.com>
2022-04-25 10:05:44 +0900

    post.js: add createPost and getPost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,21 @@
-export const getPosts= (req, res) => {
-    res.send('This works!');
+import PostMessage from "../models/postMessage.js"; //give access to real model
+
+export const getPosts= async (req, res) => {
+    try {
+        const postMessages= await PostMessage.find(); //find all post in data base, without await and async, find() may not find the new post
+        res.status(200).json(postMessages);
+    } catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post= req.body;
+    const newPost= new PostMessage(post);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
 }
