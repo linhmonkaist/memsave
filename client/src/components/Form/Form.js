39b67f8d806d61fd015b3dd533d2c32395a08504
linhmonkaist <nguyenlinhmon@gmail.com>
2022-05-11 23:46:02 +0900

    add form to make a post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -13,10 +13,13 @@ const Form= () => {
     const handleSubmit = () => {
 
     }
+    const clear = () => {
+
+    }
 
     return(
         <Paper className={classes.paper}>
-            <form autocomplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <form autocomplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6">
                     Creating a new Memory
                 </Typography>
@@ -24,8 +27,9 @@ const Form= () => {
                 <TextField name="title" variant="outlined" label="Title" fullWidth value= {postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value})} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value= {postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value})} />
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value= {postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value})} />
-                <div className={classes.fileInput}>
-
+                <div className={classes.fileInput}><FileBase type= "file" multiple={false} onDone= {({base64}) => setPostData({ ... postData, selectedFile: base64 })}/>
+                <Button className= {classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>clear</Button>
                 </div>
             </form>
         </Paper>
